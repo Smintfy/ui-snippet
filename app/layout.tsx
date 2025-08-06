8aa816bf8d046291e66b519b242fcb5d11aa3552
frija <lucydabestt@gmail.com>
2025-08-06 18:45:34 +0800

    og thumbnail

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,26 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: 'uisnippet',
   description: 'Collections of ui components made by smintfy.',
+  openGraph: {
+    url: 'https://uisnippet.dev',
+    siteName: 'UI Snippet',
+    description: 'Collections of ui components made by smintfy.',
+    images: [
+      {
+        url: '/og-thumbnail.png',
+        width: 1200,
+        height: 630,
+        alt: 'UI Snippet',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '@smintfy',
+    title: 'UI Snippet',
+    description: 'Collections of ui components made by smintfy.',
+    images: ['/og-thumbnail.png'],
+  },
 };
 
 export default function RootLayout({
